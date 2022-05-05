e8e25edcc275fdcc9b0e1d8b0d86497c2d8122ec
obruch <obruch@DESKTOP-0610CVP>
2022-05-05 18:17:13 +0300

    Добавьте файлы проекта.

---
+++ Lab7_LP/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_LP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] Q = new int[20];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                Q[i] = rand.Next(1, 20);
+                listBox1.Items.Add("Q[" + (i+1).ToString() + "] = " + Q[i].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            for (int i = 0; i < 20; i++) 
+                if(Q[i]%7==1 || Q[i] % 7 == 2 || Q[i] % 7 == 5)
+                listBox2.Items.Add("Q[" + (i + 1).ToString() + "] = " + Q[i].ToString());
+        }
+    }
+}
+
